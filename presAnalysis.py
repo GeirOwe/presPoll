e5678742e25e6959d40ba3490f08ca3440cb2e0d
GeirOwe <geir.owe@hotmail.com>
2019-10-26 13:25:14 +0200

    Update presAnalysis.py

--- presAnalysis.py
+++ presAnalysis.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime
 
+#use pandas to read the csv file into a DataFrame
 polls = pd.read_csv('president_primary_polls.csv')
 
-#print(polls.shape)
-#print(polls.dtypes)
+#print(polls.dtypes) - the columns names and their data types
 #print(polls.describe())
-#print(polls.head(10))
 #[]
 
 #data for DEM's
@@ -16,20 +16,22 @@ dems = polls.loc[polls["party"] == "DEM"]
 #data for REP's
 reps = polls.loc[polls["party"] == "REP"]
 
-print()
-print()
+#get the poll date from start_date (found in column 17 in all rows) - just pick the one from second row
+pollDate = str(polls.iloc[1, 17])
 
+print()
+print('The poll was done: ' + pollDate)
+print('The size of the dataset: '+ str(polls.shape))
 print()
 print('------------------------------------')
 print('      DEMOCRATS')
 print('------------------------------------')
 print()
-
-
 #print likelihood of DEMS beeing elected
 stats = dems.groupby(['candidate_name'])['pct'].mean()
 statsDem = stats.sort_values(ascending=False)
-print(statsDem[:10])
+#print the 15 highest ranked candidates
+print(statsDem[:15])
 
 print()
 print('------------------------------------')
@@ -39,14 +41,10 @@ print()
 #print likelihood of REPS beeing elected
 stats = reps.groupby(['candidate_name'])['pct'].mean()
 statsRep = stats.sort_values(ascending=False)
-print(statsRep[:10])
 
+#print the 15 highest ranked candidates
+print(statsRep[:15])
 
-#plot candidates likelihood to win
-#df = polls.groupby(['candidate_name']).pct.mean()
-#df = df.sort_values()
-#df.plot(kind = 'bar')
-#plt.show()
 
 
 
